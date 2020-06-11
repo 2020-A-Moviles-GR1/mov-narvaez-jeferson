f20e7601f358c2a9b25a6a0cf08879953e4d330d
Jeferson Narváez <alexresidentevil@gmail.com>
2020-06-11 09:03:43 -0500

    Introduccion lenguaje Kotlin

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -1,3 +1,37 @@
+import java.util.*
+
 fun main(args:Array<String>){
     print("Hola");
+   var edad=31;
+
+    var edadProfesor=31; //NO SE PUEDE REASIGNAR
+    var numero:Int;     //SE PUEDE REASIGNAR
+    //Tipos de variables
+    val nombreProfesor:String="Vicente Adrian";
+    val sueldo:Double=12.20
+    val inicialProfesor:Char='V';
+    val fechaNacimiento:Date =Date(); //Instanciar una clase
+    // when en vez de Switch
+    when(sueldo){
+        12.20-> { return print("sueldo normal")}
+        -12.20-> print("Sueldo negativo")
+        else -> println("No se reconoce el sueldo")
+    }
+    //IF de una linea como el operador ternario
+    val esSueldoMayorAlEstablecido= if(sueldo==12.20) true else false
+    calcularSueldo(29.00)
+    //Puedo poner en otro orden las variables al llamar a la funcion
+    calcularSueldo(tasa=14.00,sueldo = 4.00)
+}
+ // OJO ESTE OPCIONAL NO ES COMO EL DE TYPESCRIPT DELEY HAY QUE MANDAR COMO PARAMETRO PERO PUEDE SER NULL
+fun calcularSueldo(sueldo:Double,tasa:Double=12.00,opcion:Int?=null): Double {
+    if(opcion!=null){
+        return sueldo*tasa*opcion;
+    }else{
+        return sueldo*tasa
+    }
+}
+//Unit es como void
+fun imprimirMensaje():Unit{
+    print("")
 }
