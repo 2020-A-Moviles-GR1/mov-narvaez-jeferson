d56f409f49ff9443e30b09440dca8ab655b1a704
Jeferson Narváez <alexresidentevil@gmail.com>
2020-06-15 08:59:35 -0500

    Array , ArrayList y Operadores map y filter

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -1,4 +1,6 @@
 import java.util.*
+import java.util.function.Consumer
+import kotlin.collections.ArrayList
 
 fun main(args:Array<String>){
     print("Hola");
@@ -12,19 +14,46 @@ fun main(args:Array<String>){
     val inicialProfesor:Char='V';
     val fechaNacimiento:Date =Date(); //Instanciar una clase
     // when en vez de Switch
+
     when(sueldo){
-        12.20-> { return print("sueldo normal")}
+        12.20->  print("\nsueldo normal")
         -12.20-> print("Sueldo negativo")
         else -> println("No se reconoce el sueldo")
     }
     //IF de una linea como el operador ternario
-    val esSueldoMayorAlEstablecido= if(sueldo==12.20) true else false
+   // val esSueldoMayorAlEstablecido= if(sueldo==12.20) true else false
+
     calcularSueldo(29.00)
     //Puedo poner en otro orden las variables al llamar a la funcion
     calcularSueldo(tasa=14.00,sueldo = 4.00)
+    val arregloConstante:Array<Int> = arrayOf(1,2,3,4,5,6,7,8,9);
+    //los arreglos constantes no tienen metodos para añadir elementos
+    val arregloCumpleanos:ArrayList<Int>  = arrayListOf(1,2,3,4,5,6,7,8,9);
+    print(arregloCumpleanos);
+    arregloCumpleanos.add(100);
+    print(arregloCumpleanos)
+    arregloCumpleanos.remove(100)
+    println(arregloCumpleanos)
+//FOR EACH RETORNA Unit
+arregloCumpleanos.forEach{  println("Valor de la iteracion "+it);}
+arregloCumpleanos.forEach{fecha:Int ->  println("Valor de la iteracion "+fecha);   }
+arregloCumpleanos.forEach(Consumer { fecha:Int?->  println("Valor interacion "+fecha)})
+   val respuestaMap= arregloCumpleanos.map( { x ->
+       val y= x*-1 //AQUI PONER TODAS LAS OPERACIONES
+       return@map y} )
+
+    println(respuestaMap)
+    val respuestaMap2=arregloCumpleanos.map({x->x*-1 })
+    println(respuestaMap2)
+
+    val respuestaFilter=arregloCumpleanos.filter { x-> x>5 }
+    println(respuestaFilter)
+
+
 }
  // OJO ESTE OPCIONAL NO ES COMO EL DE TYPESCRIPT DELEY HAY QUE MANDAR COMO PARAMETRO PERO PUEDE SER NULL
-fun calcularSueldo(sueldo:Double,tasa:Double=12.00,opcion:Int?=null): Double {
+fun calcularSueldo(sueldo:Double,tasa:Double=12.00,opcion:Int?=null): Double? {
+     print("Calculando sueldo")
     if(opcion!=null){
         return sueldo*tasa*opcion;
     }else{
@@ -33,5 +62,6 @@ fun calcularSueldo(sueldo:Double,tasa:Double=12.00,opcion:Int?=null): Double {
 }
 //Unit es como void
 fun imprimirMensaje():Unit{
-    print("")
+    print("hola mundo"  );
 }
+//Tipos de arreglos
