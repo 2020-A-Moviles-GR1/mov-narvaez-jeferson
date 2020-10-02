65f8ba1893e65eacdc2520c27920319a214b6b50
Narváez Jeferson <alexresidentevil@gmail.com>
2020-10-02 01:14:01 -0500

    Proyecto Aplicaciones Web Final

--- Proyecto/NoticiasApp/app/src/main/java/com/example/noticiasapp/MainActivity.kt
+++ Proyecto/NoticiasApp/app/src/main/java/com/example/noticiasapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.noticiasapp
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
 import android.util.Log
 import android.view.View
 import android.widget.AbsListView
@@ -10,31 +11,43 @@ import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noticiasapp.modelos.Article
+import com.example.noticiasapp.modelos.ArticleDao
+import com.example.noticiasapp.modelos.Source
+import com.example.noticiasapp.servicios.DbService
 import com.example.noticiasapp.servicios.NewsApiService
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.card.*
+import kotlinx.android.synthetic.main.fragment_bottom_sheet.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var articleDao: ArticleDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var page:Int=0
         var mostrarCategorias:Boolean=false
         val categorias= listOf<String>("business","entertainment", "general" ,"health" ,"science", "sports", "technology")
         var noticias=NewsApiService.getNews(page)
+
         hsv_categorias.visibility= View.GONE
         tv_encabezado_app.text="Para Tí"
-        iniciarRecyclerView(noticias,this,rv_cards)
+        iniciarRecyclerView(noticias,this,rv_cards,"main")
+
+
         btn_para_ti.setOnClickListener {
             tv_encabezado_app.text="PARA TÍ"
             hsv_categorias.visibility= View.GONE
 
             page=0
                noticias=NewsApiService.getNews(page)
-            iniciarRecyclerView(noticias,this,rv_cards)
+            iniciarRecyclerView(noticias,this,rv_cards,"main")
 
         }
+
         btn_encabezados.setOnClickListener {
             hsv_categorias.visibility= View.VISIBLE
+
             tv_encabezado_app.text="ENCABEZADOS"
             page=0
             var categoria="business"
@@ -44,8 +57,21 @@ class MainActivity : AppCompatActivity() {
         btn_favoritos.setOnClickListener {
             tv_encabezado_app.text="FAVORITOS"
             hsv_categorias.visibility= View.GONE
+
             //CARGAR DE LA BASE DE DATOS
+            articleDao=DbService.getInstance(this).articleDao()
+            var articles=articleDao.getArticles()
+
+            var news=mutableListOf<Article>()
+            articles.forEach{
+                news.add(Article(Source(it.source_name,""),"",it.title,it.description,it.url,it.urlToImage,"",""))
+            }
+
+            iniciarRecyclerView(news,this,rv_cards,"favoritos")
         }
+
+
+
 
 
 
@@ -89,6 +115,8 @@ class MainActivity : AppCompatActivity() {
                 getCategoria(categoria, page)
             }
 
+
+
     }
     fun setFocus(btn:Button){
         var listaBotones= arrayListOf<Button>(btn_business,btn_technology,btn_sports,btn_science,btn_health,btn_entertainment,btn_general)
@@ -105,13 +133,14 @@ class MainActivity : AppCompatActivity() {
     }
     fun getCategoria(categoria:String,page:Int){
         var noticias=NewsApiService.getNewsCategoria(page,categoria)
-        iniciarRecyclerView(noticias,this,rv_cards)
+        iniciarRecyclerView(noticias,this,rv_cards,"main")
     }
     fun iniciarRecyclerView(lista:MutableList<Article>?,
                             activity:MainActivity,
-                            recycler_view:androidx.recyclerview.widget.RecyclerView
+                            recycler_view:androidx.recyclerview.widget.RecyclerView,
+                            desde:String
                             ){
-        val mi_adaptador=MyAdapter(lista,activity,recycler_view)
+        val mi_adaptador=MyAdapter(lista,activity,recycler_view,desde)
         rv_cards.adapter=mi_adaptador
         rv_cards.itemAnimator=androidx.recyclerview.widget.DefaultItemAnimator()
         rv_cards.layoutManager=androidx.recyclerview.widget.LinearLayoutManager(activity)
